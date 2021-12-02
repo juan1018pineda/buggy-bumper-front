fb7649d9214f85456fe2f170e40efba8cd9ccfbf
pineda.juan92 <pineda.juan92@gmail.com>
2021-12-01 22:35:08 -0500

    Forntend ready for demo with image upload directory updated

--- src/Components/NewCar/NewCar.jsx
+++ src/Components/NewCar/NewCar.jsx
@@ -23,7 +23,7 @@ const NewCar = ({ addModal, setAddModal, setCars }) => {
       seats: seats.value,
       bags: bags.value,
       price: parseInt(price.value) || 0,
-      image: imageName,
+      image: imageName.replace(/\s+/g, "").toLowerCase(),
       file: file || null,
     };
 
